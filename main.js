f8896e2a57312559be74fc211b097604948015de
hakim-b <hakimbouabdellah02@gmail.com>
2022-05-31 14:42:42 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css';
+
+let yourPoints = 0;
+let cpPoints = 0;
+let roundWinner = '';
+
+const roundMsg1 = document.getElementById("roundMessage1");
+const roundMsg2 = document.getElementById("roundMessage2");
+
+
